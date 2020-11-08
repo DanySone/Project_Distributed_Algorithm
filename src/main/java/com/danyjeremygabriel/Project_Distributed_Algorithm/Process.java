f3c59c50408692281b5ba7bffe26d56b3fa6ab4e
DanySone <dany.sonethavy@efrei.net>
2020-11-08 12:02:43 +0100

    Update Process.java

--- src/main/java/com/danyjeremygabriel/Project_Distributed_Algorithm/Process.java
+++ src/main/java/com/danyjeremygabriel/Project_Distributed_Algorithm/Process.java
@@ -25,15 +25,17 @@ public class Process extends UntypedAbstractActor implements Serializable {
 	private final int N; //number of the process
 	private final int id; //id of the process
 	private Members processes; //other processes' reference (arraylist)
-	private int localValue=0; //locally stored value, initially 0
-    private int localTS=0; //locally stored timestamp, initially 0
+	private int localValue; //locally stored value, initially 0
+    private int localTS; //locally stored timestamp, initially 0
     
-    private int state;
+    private int state; // process state
+    private int localseqnbr; // local sequence number: the number of operations performed so far
     
     //process constructor
 	public Process(int ID) {
         this.id = ID; //process ID
-        this.state = 0; //default value
+        this.state = 0; //default value and needed to get changed
+        this.localseqnbr = 0; //initial number of operations = 0
         
     }
 
@@ -44,11 +46,19 @@ public class Process extends UntypedAbstractActor implements Serializable {
             this.state = state;
         }
     }
+    // the "read-request" message type with the local seq number
+    static public class ReadRequest {
+        public int localseqnbr;
+        public ReadRequest(int localseqnbr) {
+            this.localseqnbr = localseqnbr;
+        }
+    }
+
+
 
     
 
 
-    
 
 
 
@@ -69,12 +79,6 @@ public class Process extends UntypedAbstractActor implements Serializable {
 
 
 
-
-
-
-	public int getSeqNbr() {
-		return this.r;
-	}
 	
 	public static Props createActor(int ID, int nb, int r, int t) {
         return Props.create(Process.class, () -> {
@@ -92,18 +96,19 @@ public class Process extends UntypedAbstractActor implements Serializable {
 	
 	@Override
 	public void onReceive(Object message) throws Throwable {
-		if (message instanceof Members) {//save the system's info
-            Members m = (Members) message;
-            processes = m;
-            log.info("p" + self().path().name() + " received processes info");
-        }
-		else if (message instanceof State) {
-            this.state = ((State) message).state;
-            if(this.state == 2) { //means the state of the process is faulty
-                log.info("P"+this.id+" is faulty");
+        if (this.state != 2) { //if not faulty
+            if (message instanceof Members) {//save the system's info
+                Members m = (Members) message;
+                processes = m;
+                log.info("p" + self().path().name() + " received processes info");
             }
-            
+            else if (message instanceof State) { //crash message
+                this.state = ((State) message).state;
+                if(this.state == 2) { //means the state of the process is faulty
+                    log.info("P"+this.id+" is faulty");
+                }
+                
+            }
         }
-		
 	}
 }
